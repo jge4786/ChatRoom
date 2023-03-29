1ea6b5a2190c1bdef74b45f2a935015d2a6f2641
jge4786 <jge1584@gmail.com>
2023-03-29 15:40:34 +0900

    1. ChatBody 테스트 전 저장

--- ChatRoom/ChatRoom/ChatBody.swift
+++ ChatRoom/ChatRoom/ChatBody.swift
@@ -1,34 +1,72 @@
 import UIKit
 
-class ChatBody: UIView {
+class ChatBody: UIStackView {
     enum UIOption {
-        case shouldShowProfile
-        case hasProfile
+        case shouldHideProfile
         
-        case shouldShowName
+        case shouldHideName
         
-        case shouldShowTime
+        case shouldHideTime
         
         case isMyChat
     }
     
-    var unreadCountLabel: UILabel
-    var sentTimeLabel: UILabel
+    @IBOutlet weak var profileWrapperView: UIView!
+    @IBOutlet weak var profileButton: UIButton!
     
-    var profileButton: UIButton
-    var nameLabel: UILabel
+    @IBOutlet weak var bubbleWrapper: UIView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var chatBubbleView: UIView!
     
-    var chatBubbleWrapper: UIView
-    var opacityFilter: UIView
-    var chatBubbleButton: UIButton
-    var chatBubbleText: UITextView
+    @IBOutlet weak var chatBubbleButton: UIButton!
+    @IBOutlet weak var chatBubbleTextView: UITextView!
+    @IBOutlet weak var opacityFilterView: UIView!
+    @IBOutlet weak var chatBubbleHeight: NSLayoutConstraint!
+    @IBOutlet weak var chatBubbleMaxWidth: NSLayoutConstraint!
     
+    @IBOutlet weak var infoWrapper: UIView!
+    @IBOutlet weak var unreadCountLabel: UILabel!
+    @IBOutlet weak var sentTimeLabel: UILabel!
+    
+    func setMyChatUI() {
+        profileWrapperView.isHidden = true
+        nameLabel.isHidden = true
+        
+        NSLayoutConstraint.activate([
+            infoWrapper.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            infoWrapper.trailingAnchor.constraint(equalTo: bubbleWrapper.leadingAnchor),
+        ])
+    }
+    
+    func setChatUI(options: UIOption...) -> UIStackView {
+        for item in options {
+            switch item {
+            case .isMyChat:
+                print("isMyChat")
+            case .shouldHideProfile:
+                profileWrapperView.isHidden = true
+                print("show")
+            case .shouldHideName:
+                nameLabel.isHidden = true
+                print("name")
+            case .shouldHideTime:
+                sentTimeLabel.isHidden = true
+                print("time")
+            @unknown default:
+                break
+            }
+        }
+        
+        return self
+    }
     
     init(_ frame: CGRect) {
         super.init(frame: frame)
+        
+        chatBubbleMaxWidth.constant = Constants.deviceSize.width * Constants.chatMaxWidthMultiplier
     }
     
-    required init?(coder: NSCoder) {
+    required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
